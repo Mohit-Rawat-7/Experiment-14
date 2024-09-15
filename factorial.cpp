90e0b877a2c7b35a81372ae884b3d1c566b89937
Mohit-Rawat-7 <mohit.rawat.btech2023@sitpune.edu.in>
2024-09-15 23:04:28 +0530

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,21 @@
+//Mohit Rawat
+//23070123086
+#include<iostream>
+using namespace std;
+
+int factorial(int n);
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "Factorial of " << n << " = " << factorial(n);
+    return 0;
+}
+
+int factorial(int n) {
+    if(n==0){
+        return 1;
+    } else{
+        return n*factorial(n - 1);
+    }
+}
